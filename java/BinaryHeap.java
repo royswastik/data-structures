e0386054eed2320af1f9b0d889fdbbc138e01bb2
Swastik Roy <swastik.roy@live.com>
2016-07-03 12:21:09 +0530

    Create BinaryHeap.java

---
+++ java/BinaryHeap.java
@@ -0,0 +1,28 @@
+class BinaryHeap{
+
+    static class MinHeap{
+        final public int[] array;
+        int size;
+        final int maxSize;
+        
+        public MinHeap(int maxSize){
+            this.size = 0;
+            this.array = new int[size];
+            this.maxSize = maxSize;
+        }
+        
+        public void heapify(){
+          
+        }
+        
+        public insert(){
+        }
+        
+        public delete(){
+        }
+        
+    }
+    
+    
+
+}
